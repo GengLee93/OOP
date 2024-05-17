273edbc59707eb15596b4dea696a5ad8247e8c03
何von <henry387887@gmail.com>
2024-05-17 11:17:08 +0800

    new

--- LeistungsstarkesGameFramework/Source/Game/BaseLevel.cpp
+++ LeistungsstarkesGameFramework/Source/Game/BaseLevel.cpp
@@ -10,37 +10,38 @@ void BaseLevel::SetLevel(int level)
     if (level == 1)
     {
         // normal stair
-        stairs_probability = {1, 0, 0, 0, 0, 0};
-        stairs_speed = 4;
+        stairs_probability = {0.99, 0., 0, 0, 0.01, 0};
+        stairs_speed = 3;
     }
     if (level == 2)
     {
         // normal stair and fake stair
-        stairs_probability = {0.7, 0, 0, 0, 0.3, 0};
-        stairs_speed = 4;
+        stairs_probability = {0, 0.2, 0, 0, 0., 0.8};
+        stairs_speed = 5;
+        
     }
     if (level == 3)
     {
         // normal stair, fake stair and conveyor
-        stairs_probability = {0.55, 0, 0.075, 0.075, 0.3, 0};
-        stairs_speed = 5;
+        stairs_probability = {0.75, 0.05, 0.05, 0.05, 0.05, 0.05};
+        stairs_speed = 6;
     }
     if (level == 4)
     {
         // normal stair, fake stair, conveyor and trampoline
-        stairs_probability = {0.55, 0, 0.075, 0.075, 0.2, 0.1};
-        stairs_speed = 5;
+        stairs_probability = {0.55, 0, 0.05, 0.0, 0.2, 0.2};
+        stairs_speed = 7;
     }
     if (level == 5)
     {
         // normal stair, fake stair, conveyor, trampoline and nail
-        stairs_probability = {0.3, 0.25, 0.1, 0.1, 0.15, 0.1};
+        stairs_probability = {0, 0.2, 0.4, 0.4, 0, 0};
         stairs_speed = 6;
     }
     if (level == 6)
     {
         stairs_probability = {0.25, 0.25, 0.125, 0.125, 0.25, 0.1};
-        stairs_speed = 6;
+        stairs_speed = 7;
     }
 }
 
